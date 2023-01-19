bfaea8ba0db5f632e5951c7bd572970599ef0365
cunzhiwang <2539029477@qq.com>
2023-01-19 15:31:53 +0800

    test: mvc Test

--- card-game/card-system/card-system-biz/src/test/java/com/card/game/groovy/SysCommonControllerTest.groovy
+++ card-game/card-system/card-system-biz/src/test/java/com/card/game/groovy/SysCommonControllerTest.groovy
@@ -1,21 +1,29 @@
 package com.card.game.groovy
 
+import com.card.game.common.result.Result
 import com.card.game.controller.SysCommonController
 import com.card.game.mapper.SysImageInfoMapper
 import com.card.game.pojo.vo.ImageInfoVO
 import com.card.game.service.SysImageInfoService
 import org.junit.Assert
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.reactive.server.FluxExchangeResult
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.http.HttpStatus;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.http.MediaType
+import org.spockframework.spring.SpringExtension
+import com.sun.org.apache.xerces.internal.parsers.SecurityConfiguration
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static org.mockito.Mockito.when;
 
@@ -28,46 +36,38 @@ import javax.annotation.Resource
  * @author cunzhiwang
  * @Date 2023/1/18 10:50
  */
-@WebMvcTest(SysCommonController.class)
-@ContextConfiguration(classes = SysImageInfoMapper.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class SysCommonControllerTest extends Specification {
-    @Resource
-    MockMvc mockMvc;
+    @Autowired
+    private WebTestClient webTestClient;
 
     @MockBean
     SysImageInfoService sysImageInfoService;
 
-    def "getProfilePhotos"() {
+    @Unroll
+    def "getProfilePhotos #CASE"() {
         given:
-        if (excption != null) {
-            when(sysImageInfoService.getProfilePhotos()).thenThrow(excption)
-
-        } else {
-            def vo = new ImageInfoVO(country: "test", imageInfo: null);
-            List<ImageInfoVO> mockList = [vo];
-            when(sysImageInfoService.getProfilePhotos()).thenReturn(list);
-
+        def vo = new ImageInfoVO(country: "test", imageInfo: null);
+        List<ImageInfoVO> mockList = [vo];
+        if (conditon == 1) {
+            when(sysImageInfoService.getProfilePhotos()).thenReturn(mockList);
+        } else if (conditon == 2) {
+            when(sysImageInfoService.getProfilePhotos()).thenReturn(null);
         }
-        when:
-        def result = null;
-        def catExp
-        try {
-            result = mockMvc.perform(get("/sys/getProfilePhotos").contentType(MediaType.APPLICATION_JSON))
-        } catch (Exception e) {
-            catExp = e;
-        }
-        then:
-        if (result == null) {
-            Assert.assertThrows(excption, { ->
-                throw catExp;
-            })
-        } else {
-            result.andDo(MockMvcResultHandlers.print())
-                    .andExpect(status().is(HttpStatus.UNAUTHORIZED.value()))
-        }
+        expect:
+        WebTestClient.ResponseSpec result = webTestClient.get().uri("/sys/getProfilePhotos").exchange();
+        FluxExchangeResult returnResult = result.returnResult(Result.class)
+       if(conditon==2){
+           returnResult.getStatus().INTERNAL_SERVER_ERROR.value() == status
+       }else if(conditon==1){
+           returnResult.getStatus().OK.value() == status
+       }
         where:
-        list                         | excption                       || expectedResult
-        new ArrayList<ImageInfoVO>() | IllegalArgumentException.class || "没有数据"
+        CASE | conditon | status
+        0    | 1        | 200
+        1    | 2        | 500
+
 
     }
 
