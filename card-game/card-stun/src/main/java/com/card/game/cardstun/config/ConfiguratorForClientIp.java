4101adaf7b5b87533cac935dcb87c13e60722206
cunzhiwang <2539029477@qq.com>
2023-02-01 17:48:10 +0800

    fix: websocket连接断开

--- card-game/card-stun/src/main/java/com/card/game/cardstun/config/ConfiguratorForClientIp.java
+++ card-game/card-stun/src/main/java/com/card/game/cardstun/config/ConfiguratorForClientIp.java
@@ -1,5 +1,7 @@
 package com.card.game.cardstun.config;
 
+import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpSession;
 import javax.websocket.HandshakeResponse;
 import javax.websocket.server.HandshakeRequest;
@@ -15,7 +17,9 @@ public class ConfiguratorForClientIp extends ServerEndpointConfig.Configurator {
 
     @Override
     public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
+        List<String> headers = request.getHeaders().get("Sec-WebSocket-Protocol");
         HttpSession httpSession = (HttpSession) request.getHttpSession();
+        response.getHeaders().put("Sec-WebSocket-Protocol", headers);
         //把HttpSession中保存的ClientIP放到ServerEndpointConfig中
         config.getUserProperties().put("clientIp", httpSession.getAttribute("clientIp"));
     }
