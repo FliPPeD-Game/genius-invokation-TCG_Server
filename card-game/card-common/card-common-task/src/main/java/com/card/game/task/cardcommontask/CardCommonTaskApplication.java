c4bf4b629d825d45b37332750c5b56dc4dd99e47
cunzhiwang <2539029477@qq.com>
2023-02-04 19:48:06 +0800

    feat: add task

---
+++ card-game/card-common/card-common-task/src/main/java/com/card/game/task/cardcommontask/CardCommonTaskApplication.java
@@ -0,0 +1,13 @@
+package com.card.game.task.cardcommontask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardCommonTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardCommonTaskApplication.class, args);
+    }
+
+}
---
+++ card-game/card-common/card-common-task/src/test/java/com/card/game/task/cardcommontask/CardCommonTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.card.game.task.cardcommontask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardCommonTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
