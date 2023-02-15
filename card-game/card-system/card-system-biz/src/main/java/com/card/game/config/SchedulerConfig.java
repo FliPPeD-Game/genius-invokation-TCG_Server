03ca23116970fe185094d4fae9a8469ce6df92fa
cunzhiwang <2539029477@qq.com>
2023-02-15 10:03:31 +0800

    fix: task配置

--- card-game/card-system/card-system-biz/src/main/java/com/card/game/config/SchedulerConfig.java
+++ card-game/card-system/card-system-biz/src/main/java/com/card/game/config/SchedulerConfig.java
@@ -1,5 +1,7 @@
 package com.card.game.config;
 
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
 import org.quartz.Scheduler;
 import org.quartz.ee.servlet.QuartzInitializerListener;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +11,15 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import java.io.IOException;
 
 @Configuration
+@RequiredArgsConstructor
 public class SchedulerConfig {
+    private final DataSource dataSource;
     @Bean(name="SchedulerFactory")
     public SchedulerFactoryBean schedulerFactoryBean() throws IOException {
         SchedulerFactoryBean factory = new SchedulerFactoryBean();
         factory.setAutoStartup(true);
         factory.setStartupDelay(5);//延时5秒启动
+        factory.setDataSource(dataSource);
         return factory;
     }
 
@@ -26,12 +31,6 @@ public class SchedulerConfig {
         return new QuartzInitializerListener();
     }
 
-//    /*
-//     * 通过SchedulerFactoryBean获取Scheduler的实例
-//     */
-//    @Bean(name="Scheduler")
-//    public Scheduler scheduler() throws IOException {
-//        return schedulerFactoryBean().getScheduler();
-//    }
+
 
 }
