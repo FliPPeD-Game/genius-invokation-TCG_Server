49b4084b307f57e4825ed7645298863bca1deed4
cunzhiwang <2539029477@qq.com>
2023-03-02 14:19:52 +0800

    init: 初始化数据

---
+++ card-game/sql/int.sql
@@ -0,0 +1,104 @@
+// 初始化用户卡牌
+INSERT INTO card_game.user_role_card
+(id, user_id, card_id, is_default, create_time, update_time, create_by, update_by, del_flag)
+VALUES(1, 0, 1301, 1, NULL, NULL, NULL, NULL, 0);
+INSERT INTO card_game.user_role_card
+(id, user_id, card_id, is_default, create_time, update_time, create_by, update_by, del_flag)
+VALUES(2, 0, 1103, 1, NULL, NULL, NULL, NULL, 0);
+INSERT INTO card_game.user_role_card
+(id, user_id, card_id, is_default, create_time, update_time, create_by, update_by, del_flag)
+VALUES(3, 0, 1401, 1, NULL, NULL, NULL, NULL, 0);
+
+// 初始化行为卡牌
+
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(1, 331101, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(2, 331102, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(3, 331201, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(4, 331202, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(5, 331301, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(6, 331302, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(7, 331401, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(8, 331402, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(9, 331501, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(10, 331502, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(11, 331601, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(12, 331602, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(13, 331701, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(14, 331702, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(15, 332001, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(16, 332002, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(17, 332003, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(18, 332004, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(19, 332005, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(20, 332006, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(21, 332007, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(22, 332008, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(23, 332009, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(24, 332010, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(25, 332011, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(26, 332012, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(27, 332013, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(28, 332014, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(29, 332015, '0', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO card_game.user_action_card
+(id, action_id, user_id, create_time, update_time, create_by, update_by, del_flag, is_default)
+VALUES(30, 333001, '0', NULL, NULL, NULL, NULL, 0, 0);
+
