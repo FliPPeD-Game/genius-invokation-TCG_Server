20324da91053f6448bd3897ee7113b90f488e7e1
cunzhiwang <cunzhiwang@tencent.com>
2023-03-07 22:20:39 +0800

    fix: 字段策略

--- card-game/card-common/card-common-core/src/main/java/com/card/game/common/base/BaseEntity.java
+++ card-game/card-common/card-common-core/src/main/java/com/card/game/common/base/BaseEntity.java
@@ -12,19 +12,19 @@ import java.time.LocalDateTime;
 @Data
 public class BaseEntity implements Serializable {
     @ApiModelProperty("创建时间")
-    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
     @ApiModelProperty("修改时间")
-    @TableField(fill = FieldFill.INSERT)
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
     @ApiModelProperty("创建者")
-    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @TableField(fill = FieldFill.INSERT)
     private String createBy;
 
     @ApiModelProperty("更新人")
-    @TableField(fill = FieldFill.INSERT)
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private String updateBy;
 
     @ApiModelProperty("0-正常，1-删除")
